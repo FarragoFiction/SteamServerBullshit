b5dbccef336f24025646e55d418e2b3ca2603ffd
jadedResearcher <gtg287y@gmail.com>
2019-01-30 19:16:08 -0500

    have a new http depenedency lets check this bad boy out

--- bin/server.dart
+++ bin/server.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 import 'dart:async';
+import 'package:http/http.dart' as http;
+
 
 Future main() async {
   var server = await HttpServer.bind(
@@ -9,8 +11,14 @@ Future main() async {
   print('Listening on localhost:${server.port}');
 
   await for (HttpRequest request in server) {
+    testGetting();
     request.response
       ..write('Hello, world!')
       ..close();
   }
+}
+
+void testGetting() {
+  http.read("http://farragofiction.com/WranglerNewsposts/jadedResearcher.txt").then(print);
+
 }
